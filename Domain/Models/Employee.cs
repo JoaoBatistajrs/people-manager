a8b201aaea02a253fe9df5cb54e5efe729d5a62b
João Batista Santos Junior <joao.santos@programmers.com.br>
2023-03-01 10:19:44 -0300

    Add Models, Enums and Context.

---
+++ Domain/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Models
+{
+    public class Employee
+    {
+        public Employee() { }
+        public Employee(int id, string name, string adress, string phone, string professionalEmail, string department, decimal salary, DateTime admissionDate)
+        {
+            Id = id;
+            Name = name;
+            Adress = adress;
+            Phone = phone;
+            ProfessionalEmail = professionalEmail;
+            Department = department;
+            Salary = salary;
+            AdmissionDate = admissionDate;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string ProfessionalEmail { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public DateTimeOffset? AdmissionDate { get; set; }
+    }
+}
+
---
+++ Domain/Models/EmployeeLog.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Enums;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Text.Json;
+
+namespace Domain.Models
+{
+    public class EmployeeLog : Employee, ITableEntity
+    {
+
+        public EmployeeLog(Employee employee, LogAction logAction, string partitionKey, string rowKey)
+        {
+            base.Id = employee.Id ;
+            base.Name = employee.Name;
+            base.Adress = employee.Adress;
+            base.Phone = employee.Phone;
+            base.ProfessionalEmail = employee.ProfessionalEmail;
+            base.Department = employee.Department;
+            base.Salary = employee.Salary;
+            base.AdmissionDate = employee.AdmissionDate;
+            LogAction = logAction;
+            JSON = JsonSerializer.Serialize(employee);
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+        }
+
+        public LogAction LogAction { get; set; }
+        public string JSON { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        DateTimeOffset ITableEntity.Timestamp { get; set; }
+        string ITableEntity.ETag { get; set; }
+
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
